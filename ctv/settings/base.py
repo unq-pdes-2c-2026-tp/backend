396c9ba578ad3d1d258fe1ecf021a88615041007
Pablo Pissi <pablopissi@gmail.com>
2026-08-19 22:12:55 -0300

    Agrego modelo para usuario (#1)
    * Agrego modelo para usuario
    
    * corro precommit

--- ctv/settings/base.py
+++ ctv/settings/base.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "ctv",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -103,3 +103,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,1 +1,13 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+from users.constants import UserType
+from users.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    user_type = models.PositiveSmallIntegerField(choices=UserType.choices)
+    objects = UserManager()
+    USERNAME_FIELD = "email"  # Identifies user via email
+    REQUIRED_FIELDS = []  # Additional fields for createsuperuser
