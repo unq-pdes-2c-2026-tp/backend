9b651c35bc245f296f7c0ca9778fe6ba06485f1c
Pablo Pissi <pablopissi@gmail.com>
2026-09-01 22:58:50 -0300

    Mini refactor

--- test_utils/views.py
+++ test_utils/views.py
@@ -2,43 +2,42 @@ from rest_framework.test import APIClient
 
 
 def post(url, data, fmt="json", user=None, **kwargs):
-    client = APIClient()
-    if user:
-        client.force_authenticate(user)
+    client = _get_client(user)
     response = client.post(url, data, format=fmt, **kwargs)
     return response
 
 
 def delete(url, user=None):
-    client = APIClient()
-    if user:
-        client.force_authenticate(user)
+    client = _get_client(user)
     response = client.delete(url)
     return response
 
 
 def patch(url, data, fmt="json", user=None):
-    client = APIClient()
-    if user:
-        client.force_authenticate(user)
+    client = _get_client(user)
     response = client.patch(url, data=data, format=fmt)
     return response
 
 
 def put(url, data, fmt="json", user=None):
-    client = APIClient()
-    if user:
-        client.force_authenticate(user)
+    client = _get_client(user)
     response = client.put(url, data=data, format=fmt)
     return response
 
 
-def get(url, params=None):
+def get(url, params=None, user=None):
     params = params or {}
 
-    client = APIClient()
+    client = _get_client(user)
     full_url = url
     if params:
         full_url += "?" + "&".join([f"{key}={value}" for key, value in params.items()])
     response = client.get(full_url)
     return response
+
+
+def _get_client(user) -> APIClient:
+    client = APIClient()
+    if user:
+        client.force_authenticate(user)
+    return client
