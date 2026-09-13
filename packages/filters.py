c2e9b16262f7eac8acb2462f3c166ad1c27978d4
Pablo Pissi <pablopissi@gmail.com>
2026-09-13 20:29:10 -0300

    Agrego endpoint de hoteles y comando para crearlos

--- packages/filters.py
+++ packages/filters.py
@@ -1,6 +1,7 @@
+from django.db.models import Q
 from django_filters import rest_framework as filters
 
-from packages.models import Agency
+from packages.models import Agency, Hotel
 
 
 class AgencyFilterSet(filters.FilterSet):
@@ -12,3 +13,19 @@ class AgencyFilterSet(filters.FilterSet):
 
     def search_func(self, qs, name, value):
         return qs.filter(name__icontains=value)
+
+
+class HotelFilterSet(filters.FilterSet):
+    search = filters.CharFilter(method="search_func")
+
+    class Meta:
+        model = Hotel
+        fields = ()
+
+    def search_func(self, qs, name, value):
+        search_filter = (
+            Q(name__icontains=value)
+            | Q(description__icontains=value)
+            | Q(city__name__icontains=value)
+        )
+        return qs.filter(search_filter)
--- packages/urls.py
+++ packages/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
 
-from packages.views import AgencyViewSet
+from packages.views import AgencyViewSet, HotelViewSet
 
 router = routers.SimpleRouter()
 router.register(r"agencies", AgencyViewSet)
+router.register(r"hotels", HotelViewSet)
 
 urlpatterns = router.urls
