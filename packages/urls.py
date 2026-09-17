99e4dcdb1c33ccf185d6c2bcba53dcb88b4a8414
Fede <42482452+fedejmartinez@users.noreply.github.com>
2026-09-17 10:56:23 -0300

    Creación de paquetes (#39)
    * Creación de paquetes
    
    * Creación de paquetes

--- packages/urls.py
+++ packages/urls.py
@@ -1,9 +1,13 @@
+from django.urls import path
 from rest_framework import routers
 
-from packages.views import AgencyViewSet, HotelViewSet
+from packages.views import AgencyViewSet, FlightListView, HotelViewSet, PackageViewSet
 
 router = routers.SimpleRouter()
 router.register(r"agencies", AgencyViewSet)
 router.register(r"hotels", HotelViewSet)
+router.register(r"packages", PackageViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("flights/", FlightListView.as_view(), name="flight-list")
+] + router.urls
